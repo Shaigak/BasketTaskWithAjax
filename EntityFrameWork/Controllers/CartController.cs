cafc7eacad26df86e96366b6e2b1aefbff0400f9
[shaigak] <shaigak@code.edu.az>
2023-04-17 00:26:44 +0300

    ajax

---
+++ EntityFrameWork/Controllers/CartController.cs
@@ -0,0 +1,111 @@
+﻿using EntityFrameWork.Data;
+using EntityFrameWork.Models;
+using EntityFrameWork.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace EntityFrameWork.Controllers
+{
+    public class CartController : Controller
+    {
+
+
+        private readonly AppDbContext _context;
+
+        public CartController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public async Task<IActionResult> Index()
+        {
+
+            List<BasketVM> basketProducts= GetBasketDatas();
+          
+            List<BasketDetailVM> basketDetails = new();
+
+            foreach (var item in basketProducts)
+            {
+                Product? dbProduct = await _context.Products.Include(m => m.Images).FirstOrDefaultAsync(m => m.Id == item.Id);
+
+                basketDetails.Add(new BasketDetailVM
+                {
+                    Id = dbProduct.Id,
+                    Name=dbProduct.Name,
+                    Description=dbProduct.Description,
+                    Price=dbProduct.Price,
+                    Count=item.Count,
+                    Image=dbProduct.Images.Where(m=>m.IsMain).FirstOrDefault()?.Image,
+                    Total=item.Count*dbProduct.Price
+
+                });
+            }
+             
+            return View(basketDetails);
+        }
+
+
+        public List<BasketVM> GetBasketDatas()
+        {
+            List<BasketVM> basket;
+
+            if (Request.Cookies["basket"] != null)
+            {
+                basket = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+            }
+            else
+            {
+                basket = new List<BasketVM>();
+            }
+
+
+            return basket;
+        }
+
+        [ActionName("Delete")]
+        public IActionResult DeleteProductFromBasket(int? id)
+        {
+
+            if (id is null) return BadRequest();
+
+            List<BasketVM>? basketProducts = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+
+            BasketVM? deletedProduct = basketProducts.FirstOrDefault(m => m.Id == id);
+
+            basketProducts.Remove(deletedProduct);
+
+            Response.Cookies.Append("basket", JsonConvert.SerializeObject(basketProducts));
+
+            return Ok(basketProducts);
+        }
+
+
+
+        public IActionResult MinusProductFromBasket(int? id)
+        {
+            if (id is null) return BadRequest();
+            List<BasketVM>? basketProducts = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+            var basketCount = basketProducts.FirstOrDefault(m => m.Id == id).Count--;
+            Response.Cookies.Append("basket", JsonConvert.SerializeObject(basketProducts));
+            return Ok(basketCount);
+
+
+        }
+
+        public IActionResult PlusProductFromBasket(int? id)
+        {
+            if (id is null) return BadRequest();
+            List<BasketVM>? basketProducts = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+            var basketCount = basketProducts.FirstOrDefault(m => m.Id == id).Count++;
+            Response.Cookies.Append("basket", JsonConvert.SerializeObject(basketProducts));
+            return Ok(basketCount);
+
+
+        }
+
+    }
+
+}
---
+++ EntityFrameWork/Controllers/HomeController.cs
@@ -0,0 +1,143 @@
+﻿using EntityFrameWork.Data;
+using EntityFrameWork.Models;
+using EntityFrameWork.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace EntityFrameWork.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            HttpContext.Session.SetString("name", "Pervin"); 
+            Response.Cookies.Append("surname", "Rehimli" , new CookieOptions { MaxAge = TimeSpan.FromMinutes(30)});
+
+           
+         
+
+            //Response.Cookies.Append("book",JsonConvert.SerializeObject(book));
+
+            List<Slider> sliders = _context.Sliders.ToList();
+            SliderInfo sliderInfo = _context.SliderInfos.FirstOrDefault();
+            IEnumerable<Blog> blogs = _context.Blogs.Where(m => !m.SoftDelete).ToList();
+            IEnumerable<Category>categories=_context.Categories.Where(m => !m.SoftDelete).ToList();
+            IEnumerable<Product>products=_context.Products.Include(m=>m.Images).Where(m => !m.SoftDelete).ToList();
+            IEnumerable<About>abouts= _context.Abouts.Where(m => !m.SoftDelete).ToList();
+            IEnumerable<Advantage> advantages = _context.Advantages.Where(m => !m.SoftDelete).ToList();
+            IEnumerable<Flower> flowers = _context.Flowers.Where(m => !m.SoftDelete).ToList();
+            IEnumerable<Workers> workers = _context.Workers.Where(m => !m.SoftDelete).ToList();
+            IEnumerable<Florist> florists = _context.Florists.Where(m => !m.SoftDelete).ToList();
+            IEnumerable<Instagram> instagrams = _context.Instagrams.Where(m => !m.SoftDelete).ToList();
+            IEnumerable<Subscribe> subscribes = _context.Subscribes.Where(m => !m.SoftDelete).ToList();
+            HomeVm model = new()
+            {
+                Sliders = sliders,
+                Sliderİnfo = sliderInfo,
+                Blogs=blogs,
+                Categories=categories,
+                Products=products,
+                Abouts=abouts,
+                Advantages=advantages,
+                Flowers=flowers,
+                Workers=workers,
+                Florists = florists,
+                Instagrams=instagrams,
+                Subscribes=subscribes
+                
+            };
+
+
+
+            return View(model);
+        }
+
+        [HttpPost]
+        
+        public async Task<IActionResult> AddBasket(int? id)
+        {
+            if (id is  null) return BadRequest();
+
+            Product? dbProduct = await GetProductById((int)id);
+
+            if (dbProduct == null) return NotFound();
+
+            List<BasketVM> basket = GetBasketDatas();
+
+
+            BasketVM? existProduct=basket?.FirstOrDefault(m=>m.Id==dbProduct.Id);
+
+            SetDatasToBasket (existProduct,dbProduct,basket);
+
+            return Ok();
+        }
+
+        public async Task<Product> GetProductById(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+
+        public List<BasketVM> GetBasketDatas()
+        {
+            List<BasketVM> basket;
+
+            if (Request.Cookies["basket"] != null)
+            {
+                basket = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+            }
+            else
+            {
+                basket = new List<BasketVM>();
+            }
+
+            return basket;
+        }
+
+        private void SetDatasToBasket(BasketVM existProduct, Product dbProduct,List<BasketVM> basket)
+        {
+            if (existProduct == null)
+            {
+
+                basket?.Add(new BasketVM
+                {
+                    Id = dbProduct.Id,
+                    Count = 1,
+
+
+                });
+            }
+            else
+            {
+                existProduct.Count++;
+            }
+
+            Response.Cookies.Append("basket", JsonConvert.SerializeObject(basket));
+
+        }
+
+
+    }
+
+        //public IActionResult Test()
+        //{
+        //    var sessionData = HttpContext.Session.GetString("name");
+        //    var cokieData = Request.Cookies["surname"];
+        //    var objectData = JsonConvert.DeserializeObject<Book>(Request.Cookies["book"]);
+
+        //    return Json(objectData);
+        //}
+    }
+
+
